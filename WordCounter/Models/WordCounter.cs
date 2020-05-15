25141f91476a8626e4e456e67d0a678c0347ad7e
Christopher <CHuber42.work@gmail.com>
2020-05-15 11:17:45 -0700

    brainstorm possible solutions to core counting spec (see bottom of WordCounter.cs for details). Going to research pros/cons of each

--- WordCounter/Models/WordCounter.cs
+++ WordCounter/Models/WordCounter.cs
@@ -14,8 +14,17 @@ namespace WordCounter.Models
 
     public int CountOccurrences()
     {
-      
+
       return TemplateString.Length;
     }
   }
 }
+
+
+//Spec 6 options:
+//1. .Split template into array using counter as argument, return array.length - 1 as result || 
+//2. Take scanningstring.length, use as argument in sub-string accessor for comparison
+//EXAMPLE: "Banana", "ana"; for (index < "Banana".Length - "ana".Length - 1; index++) {if ("Banana"[i:i+"ana".length] = "ana") {increase counter by 1}} counter becomes 2
+//3. Using Select.Count arguments to auto-scan for substrings.
+
+//Requires research.
